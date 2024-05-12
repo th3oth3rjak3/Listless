5bbf4443b02180cd5438c188f24c3014d686638f
Jake Hathaway <jake.d.hathaway@gmail.com>
2024-05-12 02:48:10 -0700

    Remove unused platforms

--- Listless/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace Listless;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
