240f7f644dbe4fa22c9e95cc716406aa9383a4cc
Jake Hathaway <jake.d.hathaway@gmail.com>
2024-05-19 02:13:18 -0700

    Delete unused projects

--- Listless.Domain/Models/UserList.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Listless.Domain.Models;
-
-/// <summary>
-/// A list of items that a user has created.
-/// </summary>
-/// <param name="Id">The unique id of the list from the database.</param>
-/// <param name="Name">The name of the list.</param>
-/// <param name="Items">A collection of list items.</param>
-public record UserList(int Id, string Name, IEnumerable<ListItem> Items);
